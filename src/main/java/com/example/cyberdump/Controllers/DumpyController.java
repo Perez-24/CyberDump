08d24373a71880de675ef1ac6cfb1ceb242989ab
Eugenio <perez.tno@gmail.com>
2024-06-17 15:18:31 -0500

    Update JSON

--- src/main/java/com/example/cyberdump/Controllers/DumpyController.java
+++ src/main/java/com/example/cyberdump/Controllers/DumpyController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class DumpyController {
 
-    //@Value("${secretparam}")
+    //@Value("${secretparam}"):q
+
+
     //private String secretparam;
 
     //private final UserInfoRepository userInfoRepository;
