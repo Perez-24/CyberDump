c0483f3df0595706df0ac928544a4d447e44e41f
Eugenio <perez.tno@gmail.com>
2024-08-18 21:15:02 -0600

    updating entities to match what's in DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.xerial:sqlite-jdbc:3.41.2.2")
 	implementation("org.hibernate.orm:hibernate-community-dialects:6.4.1.Final")
-	implementation("org.hibernate:hibernate-core:6.6.3.Final")
-	implementation("org.hibernate:hibernate-entitymanager:5.6.15.Final")
+//	implementation("org.hibernate:hibernate-core:6.6.3.Final")
+//	implementation("org.hibernate:hibernate-entitymanager:5.6.15.Final")
+	implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.2")
+
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework:spring-webflux")
--- src/main/java/com/example/cyberdump/Entities/ToonArmor.java
+++ src/main/java/com/example/cyberdump/Entities/ToonArmor.java
@@ -7,7 +7,42 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "CyberDB_toon_armr")
 public class ToonArmor {
+
     @Id
     private Long toon_id;
+    private Integer head;
+    private Integer body;
+    private Integer shield;
+    public Long getToon_id() {
+        return toon_id;
+    }
+
+    public void setToon_id(Long toon_id) {
+        this.toon_id = toon_id;
+    }
+
+    public Integer getHead() {
+        return head;
+    }
+
+    public void setHead(Integer head) {
+        this.head = head;
+    }
+
+    public Integer getBody() {
+        return body;
+    }
+
+    public void setBody(Integer body) {
+        this.body = body;
+    }
+
+    public Integer getShield() {
+        return shield;
+    }
+
+    public void setShield(Integer shield) {
+        this.shield = shield;
+    }
 
 }
