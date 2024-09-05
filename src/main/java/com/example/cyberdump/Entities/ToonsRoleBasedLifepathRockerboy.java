1606e04eb3cc7d2236ce80241a330c1c6cf84c7e
Eugenio <perez.tno@gmail.com>
2024-09-05 02:04:47 -0600

    ROckerboy table type conversion for entities

--- src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathRockerboy.java
+++ src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathRockerboy.java
@@ -31,35 +31,35 @@ public class ToonsRoleBasedLifepathRockerboy {
         this.rockerboy_type = rockerboy_type;
     }
 
-    public Integer getWhere_did_you_perform() {
+    public String getWhere_did_you_perform() {
         return where_did_you_perform;
     }
 
-    public void setWhere_did_you_perform(Integer where_did_you_perform) {
+    public void setWhere_did_you_perform(String where_did_you_perform) {
         this.where_did_you_perform = where_did_you_perform;
     }
 
-    public Integer getAre_you_group_or_solo() {
+    public String getAre_you_group_or_solo() {
         return are_you_group_or_solo;
     }
 
-    public void setAre_you_group_or_solo(Integer are_you_group_or_solo) {
+    public void setAre_you_group_or_solo(String are_you_group_or_solo) {
         this.are_you_group_or_solo = are_you_group_or_solo;
     }
 
-    public Integer getWere_you_once_in_a_group() {
+    public String getWere_you_once_in_a_group() {
         return were_you_once_in_a_group;
     }
 
-    public void setWere_you_once_in_a_group(Integer were_you_once_in_a_group) {
+    public void setWere_you_once_in_a_group(String were_you_once_in_a_group) {
         this.were_you_once_in_a_group = were_you_once_in_a_group;
     }
 
-    public Integer getWhy_did_you_leave() {
+    public String getWhy_did_you_leave() {
         return why_did_you_leave;
     }
 
-    public void setWhy_did_you_leave(Integer why_did_you_leave) {
+    public void setWhy_did_you_leave(String why_did_you_leave) {
         this.why_did_you_leave = why_did_you_leave;
     }
 }
