7903e11842fa046827481298159ca300a3af3238
Eugenio <perez.tno@gmail.com>
2024-09-22 02:54:38 -0600

    Tech Entity setup

--- src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathTech.java
+++ src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathTech.java
@@ -9,4 +9,10 @@ import jakarta.persistence.Table;
 public class ToonsRoleBasedLifepathTech {
     @Id
     private Long toon_id;
+    private String what_kind_of_tech;
+            private String partner_whois;
+    private String main_clients_whois;
+            private String supplies_whereget;
+    private Integer gunning4u_whois;
+
 }
