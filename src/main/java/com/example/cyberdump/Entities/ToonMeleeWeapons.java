c0d745ccb3f3a37e7cf18d133d29e1a514823699
Eugenio <perez.tno@gmail.com>
2024-10-01 03:26:01 -0600

    updated mle_wpn entity

--- src/main/java/com/example/cyberdump/Entities/ToonMeleeWeapons.java
+++ src/main/java/com/example/cyberdump/Entities/ToonMeleeWeapons.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "CyberDB_toon_mle_wpns")
+@Table(name = "mle_wpns")
 public class ToonMeleeWeapons {
 
     @Id
