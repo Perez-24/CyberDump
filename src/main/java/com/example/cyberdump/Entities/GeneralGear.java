ca5752e8bb2efc4a1a904ae40a5ca529114e83cd
Eugenio <perez.tno@gmail.com>
2024-10-05 15:46:21 -0600

    Getters and setters for item entitiy

--- src/main/java/com/example/cyberdump/Entities/GeneralGear.java
+++ src/main/java/com/example/cyberdump/Entities/GeneralGear.java
@@ -10,6 +10,47 @@ public class GeneralGear {
     @Id
     private Integer gear_id;
     private String gear_name;
-    private Integer cost;private String gear_desc;
+    private Integer cost;
+    private String gear_desc;
     private String gear_desc_2;
+
+    public Integer getGear_id() {
+        return gear_id;
+    }
+
+    public void setGear_id(Integer gear_id) {
+        this.gear_id = gear_id;
+    }
+
+    public String getGear_name() {
+        return gear_name;
+    }
+
+    public void setGear_name(String gear_name) {
+        this.gear_name = gear_name;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public String getGear_desc() {
+        return gear_desc;
+    }
+
+    public void setGear_desc(String gear_desc) {
+        this.gear_desc = gear_desc;
+    }
+
+    public String getGear_desc_2() {
+        return gear_desc_2;
+    }
+
+    public void setGear_desc_2(String gear_desc_2) {
+        this.gear_desc_2 = gear_desc_2;
+    }
 }
