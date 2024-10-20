e37b9b818e0771368e2a66687ca71f6eafda81cd
Eugenio <perez.tno@gmail.com>
2024-10-20 01:25:55 -0600

    changed table naming convention for ExecLifepath

--- src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathExec.java
+++ src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathExec.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "CyberDB_toons_rbl_exec")
+@Table(name = "toons_rbl_exec")
 public class ToonsRoleBasedLifepathExec {
     @Id
     private Long toon_id;
