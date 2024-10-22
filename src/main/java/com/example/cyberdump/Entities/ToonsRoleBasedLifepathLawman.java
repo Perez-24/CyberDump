1573ceec86f33d61ddf0c6920beebc331271b764
Eugenio <perez.tno@gmail.com>
2024-10-22 01:27:48 -0600

    changed table naming convention for Lawman

--- src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathLawman.java
+++ src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathLawman.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "CyberDB_toons_rbl_lawman")
+@Table(name = "toons_rbl_lawman")
 public class ToonsRoleBasedLifepathLawman {
     @Id
     private Long toon_id;
