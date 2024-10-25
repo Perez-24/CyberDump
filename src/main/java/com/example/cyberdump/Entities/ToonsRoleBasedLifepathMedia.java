c03547e8c2eda7dd974db88e55f30c4e2e8e9f42
Eugenio <perez.tno@gmail.com>
2024-10-25 01:31:30 -0600

    changed table naming convention for Medtech

--- src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathMedia.java
+++ src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathMedia.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "CyberDB_toons_rbl_media")
+@Table(name = "toons_rbl_media")
 public class ToonsRoleBasedLifepathMedia {
     @Id
     private Long toon_id;
