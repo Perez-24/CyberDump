1dec3b61ea4e84076bf7c443a240fb496a37a6a0
Eugenio <perez.tno@gmail.com>
2024-11-02 01:37:40 -0600

    changed table naming convention for solo

--- src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathSolo.java
+++ src/main/java/com/example/cyberdump/Entities/ToonsRoleBasedLifepathSolo.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "CyberDB_toons_rbl_solo")
+@Table(name = "toons_rbl_solo")
 public class ToonsRoleBasedLifepathSolo {
     @Id
     private Long toon_id;
