750fa1350a6a58dab1788d3b30cb3a60a005a3dd
cubanpete24 <perez.tno@gmail.com>
2024-12-24 10:35:37 -0700

    updated Cyberware Repository to seach for Cyberware by name

--- src/main/java/com/example/cyberdump/Repository/CyberwareRepository.java
+++ src/main/java/com/example/cyberdump/Repository/CyberwareRepository.java
@@ -1,10 +1,12 @@
 package com.example.cyberdump.Repository;
 
 import com.example.cyberdump.Entities.GeneralGearRelated.Cyberware;
+import com.example.cyberdump.Entities.ToonRelated.Toons;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CyberwareRepository extends JpaRepository<Cyberware, Integer> {
+    Cyberware findByCyberware_nameIgnoreCase(String cyberware_name);
 
 }
