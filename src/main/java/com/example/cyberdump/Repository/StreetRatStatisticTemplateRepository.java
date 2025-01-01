e26f7fb009b9b36468e5ec48b36b1a7829545e4f
cubanpete24 <perez.tno@gmail.com>
2025-01-01 11:37:15 -0700

    created table for templates

---
+++ src/main/java/com/example/cyberdump/Repository/StreetRatStatisticTemplateRepository.java
@@ -0,0 +1,10 @@
+package com.example.cyberdump.Repository;
+
+import com.example.cyberdump.Entities.Core.Corporations;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StreetRatStatisticTemplateRepository extends JpaRepository<Corporations, Integer> {
+
+}
